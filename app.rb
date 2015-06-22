e0bc926847682594e264866cf8e3be8572f01a69
Tameka Roberts <TamekaCrystalDesigns@gmail.com>
2015-06-21 23:38:07 -0400

    second commit, leson 6

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models/user'
+require './models/post'
+require 'pry'
 
 set :database,"sqlite3:blogapp.sqlite3"
+
+
+get '/users' do
+	@users = User.all
+	erb :users
+	
+end
+
+get '/users/:id' do
+	@user = User.find_by_id(params[:id])
+
+	erb :user_detail
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621022050) do
+ActiveRecord::Schema.define(version: 20150621232710) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
